faf4dce61c2efd135d49c80a6f6e3856eb7300dc
Amazon Donato <ajd.oliveira@gmail.com>
2020-06-25 15:51:01 -0400

    Organização do código - Aula 4

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,46 +1,46 @@
 class Personagem extends Animacao {
-   constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
-  super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
-     
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
+
     this.variacaoY = variacaoY;
-     this.yInicial = height - this.altura - variacaoY;
-     this.y = this.yInicial;
-     
-     this.velocidadeDoPulo = 0;
-     this.gravidade = 8;
-     this.alturaDoPulo = -50
-     this.pulos = 0
+    this.yInicial = height - this.altura - variacaoY;
+    this.y = this.yInicial;
+
+    this.velocidadeDoPulo = 0;
+    this.gravidade = 3;
+    this.alturaDoPulo = -30
+    this.pulos = 0
   }
-  
+
   pula() {
-    if(this.pulos < 2) {
+    if (this.pulos < 5) {
       this.velocidadeDoPulo = this.alturaDoPulo
       this.pulos++
     }
   }
-  
+
   aplicarGravidade() {
     this.y = this.y + this.velocidadeDoPulo;
     this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
-  
-    if(this.y > this.yInicial){
-       this.y = this.yInicial
-       this.pulos = 0
+
+    if (this.y > this.yInicial) {
+      this.y = this.yInicial
+      this.pulos = 0
     }
   }
-  
+
   estaColidindo(inimigo) {
     const precisao = .7
     const colisao = collideRectRect(
-      this.x, 
-      this.y,              
-      this.largura * precisao, 
+      this.x,
+      this.y,
+      this.largura * precisao,
       this.altura * precisao,
       inimigo.x,
       inimigo.y,
       inimigo.largura * precisao,
       inimigo.altura * precisao
-    
+
     )
     return colisao;
   }
